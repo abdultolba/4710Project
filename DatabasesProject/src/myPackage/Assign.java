84fb3a8cb7559d4dcde0225ab65142adc2bca359
Anmol Multani <anmolsmultani20@gmail.com>
2018-11-29 00:33:04 -0500

    Update Assign.java

--- DatabasesProject/src/myPackage/Assign.java
+++ DatabasesProject/src/myPackage/Assign.java
@@ -6,61 +6,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Servlet implementation class Assign
  */
-@WebServlet(name="Assign", urlPatterns={"/Assign"})
+@WebServlet ( name = "Assign", urlPatterns = { "/Assign" } )
 public class Assign extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Assign() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Assign ( ) { super ( ); }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request , HttpServletResponse response)
+	 */
+	protected void doGet ( HttpServletRequest request , HttpServletResponse response ) throws ServletException, IOException { }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request , HttpServletResponse response)
+	 */
+	protected void doPost ( HttpServletRequest request , HttpServletResponse response ) throws ServletException, IOException {
+		String executionResult = "";
 
-        String selection = request.getParameter("Assign");
-        String[] selection2 = request.getParameterValues("Assign2");
-        for(int i =0; i < selection2.length; i++){
-            System.out.println(selection2[i]);
-        }
+		String selection = request.getParameter ( "Assign" );
+		String[] selection2 = request.getParameterValues ( "Assign2" );
 
-        if (selection2.length - 1 > 3){
-            String result = "Error: Assignment failure! You cannot assign more than 3 reviewers to a paper.";
-            System.out.print(result);
-            request.setAttribute("result", result);
-            request.getRequestDispatcher("Assign.jsp").forward(request, response);
-        }
-        else{
-            Init i = new Init();
-            int res = i.assign(selection, selection2);
+		System.out.println ( Arrays.toString ( selection2 ) );
 
-            if (res == 1){
-                String result = "Assignment success!";
-                request.setAttribute("result", result);
-            }
-            else{
-                String result = "Error: Assignment failure.";
-                request.setAttribute("result", result);
-            }
-            request.getRequestDispatcher("Assign.jsp").forward(request, response);
-        }
-    }
+		if ( selection2.length >= 4 )
+			executionResult = "Assignment Unsuccessful.\n At most three reviewers can be assigned to one paper.";
+		else if ( selection.isEmpty () )
+			executionResult = "Assignment Unsuccessful.\n Assign field is empty.";
+		else
+			executionResult = ( new Init ( ).assign ( selection , selection2 ) ) ? ( "Assignment was Successful") : ( "Assignment Unsuccessful. Unknown Error Occurred" );
 
+		request.setAttribute ( "result" , executionResult );
+		request.getRequestDispatcher ( "Assign.jsp" ).forward ( request , response );
+	}
 }
