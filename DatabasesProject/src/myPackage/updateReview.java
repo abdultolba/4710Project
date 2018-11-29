e1874d712dd8b54b9d6adaed555f320af72aa288
Anmol Multani <anmolsmultani20@gmail.com>
2018-11-29 00:36:14 -0500

    Update updateReview.java

--- DatabasesProject/src/myPackage/updateReview.java
+++ DatabasesProject/src/myPackage/updateReview.java
@@ -1,85 +1,56 @@
 package myPackage;
 
-import myPackage.UpdateDB;
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Servlet implementation class updateReview
  */
-@WebServlet("/updateReview")
+@WebServlet ( "/updateReview" )
 public class updateReview extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public updateReview() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public updateReview ( ) { super ( ); }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request , HttpServletResponse response)
+	 */
+	protected void doGet ( HttpServletRequest request , HttpServletResponse response ) throws ServletException, IOException { }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request , HttpServletResponse response)
+	 */
+	protected void doPost ( HttpServletRequest request , HttpServletResponse response ) throws ServletException, IOException {
+		if ( !request.getParameter ( "addnew" ).isEmpty () ) {
+			String sdateX = request.getParameter ( "sdateX" );
+			String commX = request.getParameter ( "commX" );
+			String recommendationX = request.getParameter ( "recommendationX" );
+			String paperidX = request.getParameter ( "paperidX" );
+			String memberidX = request.getParameter ( "memberidX" );
 
+			new Init ().addReview ( sdateX , commX , recommendationX , paperidX , memberidX );
+		} else {
+			String reportid = request.getParameter ( "reportid" );
+			String sdate = request.getParameter ( "sdate" );
+			String comm = request.getParameter ( "comm" );
+			String recommendation = request.getParameter ( "recommendation" );
+			String paperid = request.getParameter ( "paperid" );
+			String memberid = request.getParameter ( "memberid" );
 
-        // (update) get params
-        String reportid = request.getParameter("reportid");
-        //int paperid = Integer.parseInt(paperid2);
-        String sdate = request.getParameter("sdate");
-        String comm = request.getParameter("comm");
-        String recommendation = request.getParameter("recommendation");
-        String paperid = request.getParameter("paperid");
+			if ( !request.getParameter ( "update" ).isEmpty () )
+				new Init ().updateReview ( reportid , sdate , comm , recommendation , paperid , memberid );
 
-        String memberid = request.getParameter("memberid");
+			if ( !request.getParameter ( "delete" ).isEmpty () )
+				new Init ().deleteReview ( reportid , sdate , comm , recommendation , paperid , memberid );
+		}
 
-        String sdateX = request.getParameter("sdateX");
-        String commX = request.getParameter("commX");
-        String recommendationX = request.getParameter("recommendationX");
-        String paperidX = request.getParameter("paperidX");
-
-        String memberidX = request.getParameter("memberidX");
-
-
-        if(request.getParameter("update")!=null){
-            Init i = new Init();
-            int x = i.updateReview(reportid,sdate,comm,recommendation,paperid,memberid);
-
-            System.out.print(x);
-        }
-
-
-        if(request.getParameter("delete")!=null){
-            Init b = new Init();
-            int y = b.deleteReview(reportid,sdate,comm,recommendation,paperid,memberid);
-
-            System.out.print(y);
-
-        }
-
-        if(request.getParameter("addnew")!=null){
-            Init c = new Init();
-            int z = c.addReview(sdateX,commX,recommendationX, paperidX,memberidX);
-
-            System.out.print(z);
-        }
-
-        request.getRequestDispatcher("updateReview.jsp").forward(request, response);
-
-    }
-
+		request.getRequestDispatcher ( "updateReview.jsp" ).forward ( request , response );
+	}
 }
