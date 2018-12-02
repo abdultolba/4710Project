a81795a560daf6a5e2d6675597212f94c995389e
Anmol Multani <anmolsmultani20@gmail.com>
2018-12-02 17:58:00 -0500

    Update UpdateDB.java

--- DatabasesProject/src/myPackage/UpdateDB.java
+++ DatabasesProject/src/myPackage/UpdateDB.java
@@ -1,49 +1,29 @@
 package myPackage;
 
-import java.sql.DriverManager;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.SEVERE;
 
 public class UpdateDB {
+	private static final Logger logger = Logger.getLogger ( UpdateDB.class.getName ( ) );
 	private java.sql.Connection conn;
-
-	public int createConn ( String dbName ) {
-		try {
-			Class.forName ( "com.mysql.jdbc.Driver" ).getDeclaredConstructor ( ).newInstance ( );
-			conn = DriverManager.getConnection ( "jdbc:mysql://localhost:3306/" + dbName + "?allowMultiQueries=true&user=john&password=pass1234" );
-		} catch ( Exception e ) {
-			System.out.print ( "[ERROR][UpdateDB::createConn] - UnknownError Encountered: " + e + " while creating connection\n" );
-			e.printStackTrace ( );
-			return 0;
-		}
-
-		return 1;
-	}
-
-	public int endConn ( ) {
-		try {
-			conn.close ( );
-			return 1;
-		} catch ( SQLException e ) {
-			System.out.print ( "[ERROR][UpdateDB::endConn] - MySQL Encountered: " + e + " while closing connection\n" );
-			e.printStackTrace ( );
-			return 0;
-		}
-	}
-
+	
 	// -------––---------------------------------------------------------------
 
 	public Boolean runSQLStatement ( String sqlStatement ) {
 		Boolean queryExecutionResult = false;
-
+		logger.info ( "MySQL Statement: " + sqlStatement );
+		
 		try {
-			System.out.print ( "[INFO][UpdateDB::runSQLStatement] - MySQL Statement: " + sqlStatement );
-
-			createConn ( "samoledb" );
+			conn = new ConnectionFactory ( ).getConnection ( );
+			
 			PreparedStatement preparestatement = conn.prepareStatement ( sqlStatement );
 			queryExecutionResult = preparestatement.execute ( sqlStatement );
 		} catch ( SQLException e ) {
-			System.out.print ( "[ERROR][UpdateDB::runSQLStatement] - Exception Encountered: " + e + "\n" );
+			logger.log ( SEVERE , "Exception Encountered running SQL Statement" , e );
 			e.printStackTrace ( );
 		}
 
@@ -57,11 +37,7 @@ public class UpdateDB {
 	}
 
 	public Boolean insertPaper ( String paperid , String title , String abstr , String pdf ) {
-		return runSQLStatement ( "INSERT INTO paper VALUES("
-				                         + paperid + ", '"
-				                         + title + "', '"
-				                         + abstr + "', '"
-				                         + pdf + "');" );
+		return runSQLStatement ( "INSERT INTO paper VALUES(" + paperid + ", '" + title + "', '" + abstr + "', '" + pdf + "');" );
 	}
 
 	public Boolean deletePaper ( String paperID ) {
@@ -106,3 +82,27 @@ public class UpdateDB {
 
 	// -------––---------------------------------------------------------------
 }
+
+// 	public int createConn ( String dbName ) {
+// 		try {
+// 			Class.forName ( "com.mysql.jdbc.Driver" ).getDeclaredConstructor ( ).newInstance ( );
+// 			conn = DriverManager.getConnection ( "jdbc:mysql://localhost:3306/" + dbName + "?allowMultiQueries=true&user=john&password=pass1234" );
+// 		} catch ( Exception e ) {
+// 			System.out.print ( "[ERROR][UpdateDB::createConn] - UnknownError Encountered: " + e + " while creating connection\n" );
+// 			e.printStackTrace ( );
+// 			return 0;
+// 		}
+
+// 		return 1;
+// 	}
+
+// 	public int endConn ( ) {
+// 		try {
+// 			conn.close ( );
+// 			return 1;
+// 		} catch ( SQLException e ) {
+// 			System.out.print ( "[ERROR][UpdateDB::endConn] - MySQL Encountered: " + e + " while closing connection\n" );
+// 			e.printStackTrace ( );
+// 			return 0;
+// 		}
+// 	}
